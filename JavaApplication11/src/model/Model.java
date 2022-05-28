48798e2cf0d24c1e3e707d44dfd7044d709f9924
DESKTOP-A0GNS6A\jinah <jinah0206@naver.com>
2022-05-28 16:06:49 +0900

    모델 최신

--- JavaApplication11/src/model/Model.java
+++ JavaApplication11/src/model/Model.java
@@ -1,10 +1,7 @@
 
 package model;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> e9a8a9e7f86c266649f0a421f6f206c812bd35be
 import dao.ADao;
 import dto.AGroup;
 import dto.AJoin;
@@ -55,20 +52,12 @@ public class Model {
      public ArrayList<AJoin> listGroupMd(int num){
          return ADao.getDao().listGroup(num);
      }
-<<<<<<< HEAD
      // 내 정보 변경
-=======
-     // 내 정보 변경 (@author 현진)
->>>>>>> e9a8a9e7f86c266649f0a421f6f206c812bd35be
      public void updateMyInfoMd(AMember ref, String mloc, int mhobby){
          ADao.getDao().updateMyInfo(ref, mloc, mhobby);
      }
      
-<<<<<<< HEAD
      // 내가 가입한 모임 리스트 불러오기 
-=======
-     // 내가 가입한 모임 리스트 불러오기 (@author 현진)
->>>>>>> e9a8a9e7f86c266649f0a421f6f206c812bd35be
      public ArrayList<AJoin> listMyGroupMd(int num) {
          return ADao.getDao().listMyGroup(num);
      }
@@ -77,13 +66,8 @@ public class Model {
          ADao.getDao().addBoard(b,aj);
      }
      // 게시판 글 목록 출력
-<<<<<<< HEAD
      public ArrayList<Board> listBoardMd(AGroup group){
          return ADao.getDao().listBoard(group);
-=======
-     public ArrayList<Board> listBoardMd(AGroup num){
-         return ADao.getDao().listBoard(num);
->>>>>>> e9a8a9e7f86c266649f0a421f6f206c812bd35be
      }
      // 게시글 상세내용 출력
      public Board getDetail(Board a) {
@@ -98,7 +82,6 @@ public class Model {
        public ArrayList<BoardComm> getDetailBoardCommMd(BoardComm bc){
            return ADao.getDao().getDetailBoardComm(bc);
        }
-<<<<<<< HEAD
        // 내가 가입한 모임 탈퇴하기
      public void deleteMyMoimMd(AJoin a){
          ADao.getDao().deleteMyMoim(a);
@@ -107,6 +90,4 @@ public class Model {
       public ArrayList<AGroup> selectGroupHobby(int num) {
         return  ADao.getDao().selectGroupHobby(num);
       }
-=======
->>>>>>> e9a8a9e7f86c266649f0a421f6f206c812bd35be
 }
